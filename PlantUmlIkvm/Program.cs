4e93262f1eaa2282ffac4c82b78e6430872b939b
aryehcitron@gmail.com <aryehcitron@gmail.com>
2024-07-15 22:25:15 +0100

    Updated IKVM and now fully working

--- PlantUmlIkvm/Program.cs
+++ PlantUmlIkvm/Program.cs
@@ -1,13 +1,5 @@
-﻿using System.Reflection;
-using com.plantuml.api.cheerpj;
-using com.plantuml.api.cheerpj.v1;
-using java.io;
+﻿using java.io;
 using net.sourceforge.plantuml;
-using net.sourceforge.plantuml.api;
-using net.sourceforge.plantuml.security;
-using net.sourceforge.plantuml.sequencediagram;
-using sun.misc;
-using sun.net.www.content.image;
 
 namespace PlantUmlIkvm;
 
@@ -18,8 +10,15 @@ public class Program
         var uml = "@startuml\n";
         uml += "Bob -> Alice : hello\n";
         uml += "@enduml\n";
-        var reader = new net.sourceforge.plantuml.SourceStringReader(uml);
-        OutputStream png = new Base64OutputStream();
-        var desc = reader.outputImage(png).getDescription();
+
+        var reader = new SourceStringReader(uml);
+        var png = new ByteArrayOutputStream();
+        reader.outputImage(png);
+
+        var imageAsBytes = png.toByteArray();
+        var imageAsBase64 = Convert.ToBase64String(imageAsBytes);
+
+        System.Console.WriteLine(imageAsBase64);
+        System.Console.ReadKey();
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # PlantUmlIkvmTest
-My attempt to run PlantUml using IKVM - posted here to help aid people trying to help with my StackOverflow question: 
-
-https://stackoverflow.com/questions/77773432/running-plantuml-jar-on-ikvm-fails-with-system-typeinitializationexception
+My attempt to run PlantUml using IKVM.  Now fully working after IKVM update.  Win! :)
