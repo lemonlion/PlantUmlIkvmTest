e51eb05ca02ea12101eccc22c73809380e0c3e10
aryehcitron@gmail.com <aryehcitron@gmail.com>
2024-07-20 12:40:56 +0100

    Improvements

--- PlantUmlIkvm/Program.cs
+++ PlantUmlIkvm/Program.cs
@@ -1,5 +1,6 @@
 ﻿using java.io;
 using net.sourceforge.plantuml;
+using net.sourceforge.plantuml.klimt.sprite;
 
 namespace PlantUmlIkvm;
 
@@ -7,18 +8,51 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var uml = "@startuml\n";
-        uml += "Bob -> Alice : hello\n";
-        uml += "@enduml\n";
+        var plantUml = @"
+@startuml
+Bob -> Alice : hello
+@enduml
+".Trim();
 
-        var reader = new SourceStringReader(uml);
+        var png = WriteImage(plantUml, format: FileFormat.PNG);
+        System.Console.WriteLine($"The PlantUml image can be found at: {png.FilePath}");
+
+        var svg = WriteImage(plantUml, format: FileFormat.SVG);
+        System.Console.WriteLine($"The PlantUml image can be found at: {svg.FilePath}");
+
+        var asciiArt = WriteImage(plantUml, format: FileFormat.ATXT);
+        System.Console.WriteLine($"The PlantUml image can be found at: {asciiArt.FilePath}");
+
+        System.Console.WriteLine();
+
+        System.Console.WriteLine($"Here is the AsciiArt version: ");
+        System.Console.WriteLine(asciiArt.StringResult);
+
+        System.Console.WriteLine();
+
+        System.Console.WriteLine($"Here is the base64 version of the PNG: ");
+        System.Console.WriteLine(png.Base64);
+
+        System.Console.WriteLine();
+
+        System.Console.WriteLine($"Here is the base64 version of the SVG: ");
+        System.Console.WriteLine(svg.Base64);
+
+        System.Console.ReadKey();
+    }
+
+    private static (string FilePath, string Base64, string StringResult) WriteImage(string plantUml, FileFormat format)
+    {
+        var pngReader = new SourceStringReader(plantUml);
         var png = new ByteArrayOutputStream();
-        reader.outputImage(png);
+        pngReader.outputImage(png, new FileFormatOption(format));
 
         var imageAsBytes = png.toByteArray();
         var imageAsBase64 = Convert.ToBase64String(imageAsBytes);
+        var filepath = Path.Combine(Directory.GetCurrentDirectory(), $"plantUmlIkvm{format.getFileSuffix()}");
 
-        System.Console.WriteLine(imageAsBase64);
-        System.Console.ReadKey();
+        System.IO.File.WriteAllBytes(filepath, imageAsBytes);
+
+        return (filepath, imageAsBase64, format == FileFormat.ATXT ? System.IO.File.ReadAllText(filepath) : imageAsBase64);
     }
 }
